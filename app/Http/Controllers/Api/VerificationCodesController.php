1d296d181269598b518bc08a3aa70a676822033d
summercake@qq.com <summercake@qq.com>
2019-02-02 04:11:25 -0600

    add sms test 3

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -11,24 +11,27 @@ class VerificationCodesController extends Controller
     public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
         $phone = $request->phone;
-        $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
-        try{
-            $result = $easySms->send($phone, [
-                'content'=>"【云片网】您的验证码是{$code}。如非本人操作，请忽略本短信"
-            ]);
-        } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
-            $message = $exception->getException('yunpian')->getMessage();
+        if (!app()->environment('production')) {
+            $code = '1234';
+        } else {
+            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+            try{
+                $result = $easySms->send($phone, [
+                    'content' => "【云片网】您的验证码是{$code}。如非本人操作，请忽略本短信",
+                ]);
+            } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
+                $message = $exception->getException('yunpian')->getMessage();
 
-            return $this->response->errorInternal($message ?: '短信发送异常');
+                return $this->response->errorInternal($message ?: '短信发送异常');
+            }
         }
         $key = 'verificationCode_'.str_random(15);
         $expiredAt = now()->addMinutes(10);
         \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
 
         return $this->response->array([
-            'key'=>$key,
+            'key'        => $key,
             'expired_at' => $expiredAt->toDateTimeString(),
         ])->setStatusCode(201);
-
     }
 }
