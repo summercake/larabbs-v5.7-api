75cfebdfdd92c2e00534fd89198a0855c6e95e99
summercake@qq.com <summercake@qq.com>
2019-02-02 04:34:23 -0600

    add route throttle

--- routes/api.php
+++ routes/api.php
@@ -12,22 +12,25 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 $api = app('Dingo\Api\Routing\Router');
-
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api'
-], function($api) {
-    // 短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
-    // 用户注册
-    $api->post('users', 'UsersController@store')
-        ->name('api.users.store');
+    'namespace' => 'App\Http\Controllers\Api',
+], function ($api){
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit'      => 1,
+        'expires'    => 1,
+    ], function ($api){
+        // 短信验证码
+        $api->post('verificationCodes', 'VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
+        // 用户注册
+        $api->post('users', 'UsersController@store')
+            ->name('api.users.store');
+    });
 });
-
-$api->version('v2', function($api) {
-    $api->get('version', function() {
+$api->version('v2', function ($api){
+    $api->get('version', function (){
         return response('this is version v2');
     });
 });
